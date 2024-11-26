19cd91b3581f6bbf6ca6a97f356baf991fb8b808
bilol-salimov <salimovbilol321@gmail.com>
2024-11-26 22:39:18 +0500

    feat: check form complete for complete order

--- pages/confirm-order/js/app.js
+++ pages/confirm-order/js/app.js
@@ -1,11 +1,38 @@
 // Selectors
 const inputDate = document.querySelector("#date");
 const radios = document.querySelectorAll("input[type='radio']");
+const radioCash = document.querySelector(".cash");
+const radioCard = document.querySelector(".card");
 const closeBtn = document.querySelector(".close-btn");
 const completeBtn = document.querySelector("form .btn");
+const formInputs = document.querySelectorAll("input");
 
 // Event Listeners
 closeBtn.addEventListener("click", close);
+radioCash.addEventListener("click", (e) => {
+  removeChecked();
+  if (e.target === "input") {
+    e.target.checked = "true";
+  } else {
+    const input = e.target.closest(".cash").querySelector("input");
+    input.checked = "true";
+  }
+});
+radioCard.addEventListener("click", (e) => {
+  removeChecked();
+  if (e.target === "input") {
+    e.target.checked = "true";
+  } else {
+    const input = e.target.closest(".card").querySelector("input");
+    input.checked = "true";
+  }
+});
+
+completeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("active")) {
+  }
+});
 
 // Get date
 const today = new Date();
@@ -17,28 +44,35 @@ today.setDate(today.getDate() + 1);
 const nextDay = today.toISOString().split("T")[0];
 inputDate.value = nextDay;
 
-// Input radio checks
-function addChecked() {
-  radios.forEach((radio) => {
-    radio.addEventListener("click", (e) => {
-      removeChecked();
-      e.target.checked = "true";
-    });
-  });
-}
-
-addChecked();
-
 function removeChecked() {
   radios.forEach((radio) => {
     radio.checked = false;
   });
 }
 
-function complete() {
-  completeBtn.addEventListener("click", () => {});
-}
-
 function close() {
   window.location.pathname = "index.html";
 }
+
+// Complete button add and remove active
+function checkFormComplete() {
+  let allFilled = true;
+
+  formInputs.forEach((input) => {
+    if (input.value.trim() === "") {
+      allFilled = false;
+    }
+  });
+
+  if (allFilled) {
+    completeBtn.classList.add("active");
+    console.log("value bor");
+  } else {
+    completeBtn.classList.remove("active");
+    console.log("value bo'sh");
+  }
+}
+
+formInputs.forEach((input) => {
+  input.addEventListener("input", checkFormComplete);
+});
